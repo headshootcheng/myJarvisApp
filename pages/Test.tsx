8075c9d37d9fc748267734be54a400b05e81dfbd
petercheng <petercheng7788@gmail.com>
2021-12-19 20:35:58 +0800

    test

--- pages/Test.tsx
+++ pages/Test.tsx
@@ -5,19 +5,6 @@ import * as TaskManager from "expo-task-manager";
 
 const BACKGROUND_FETCH_TASK = "background-fetch";
 
-// 1. Define the task by providing a name and the function that should be executed
-// Note: This needs to be called in the global scope (e.g outside of your React components)
-TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
-  const now = Date.now();
-
-  console.log(
-    `Got background fetch call at date: ${new Date(now).toISOString()}`
-  );
-
-  // Be sure to return the successful result type!
-  return BackgroundFetch.BackgroundFetchResult.NewData;
-});
-
 // 2. Register the task at some point in your app by providing the same name, and some configuration options for how the background fetch should behave
 // Note: This does NOT need to be in the global scope and CAN be used in your React components!
 async function registerBackgroundFetchAsync() {
@@ -39,7 +26,18 @@ export default function BackgroundFetchScreen() {
   const [isRegistered, setIsRegistered] = React.useState(false);
   const [status, setStatus] =
     React.useState<BackgroundFetch.BackgroundFetchStatus | null>(null);
-
+  const [number, setNumber] = React.useState<number>(0);
+  // 1. Define the task by providing a name and the function that should be executed
+  // Note: This needs to be called in the global scope (e.g outside of your React components)
+  TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+    const now = Date.now();
+    setNumber((prevState) => prevState + 1);
+    console.log(
+      `Got background fetch call at date: ${new Date(now).toISOString()}`
+    );
+    // Be sure to return the successful result type!
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+  });
   React.useEffect(() => {
     checkStatusAsync();
   }, []);
@@ -80,6 +78,7 @@ export default function BackgroundFetchScreen() {
         </Text>
       </View>
       <View style={styles.textContainer}></View>
+      <Text>{number}</Text>
       <Button
         title={
           isRegistered
