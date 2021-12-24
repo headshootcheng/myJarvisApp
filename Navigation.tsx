685aada3ed87c63b6969d75a0d4478836cc8108c
petercheng <petercheng7788@gmail.com>
2021-12-25 03:17:16 +0800

    feat: finished scan qr code layout

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,53 +1,151 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  ParamListBase,
+  RouteProp,
+} from "@react-navigation/native";
+import { Image } from "react-native";
 import colors from "./constants/color";
 import Home from "./pages/Home";
 import Notification from "./pages/Notification";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useFonts } from "@expo-google-fonts/inter";
+import {
+  createDrawerNavigator,
+  DrawerNavigationOptions,
+} from "@react-navigation/drawer";
 import { HamburgerIcon, Button } from "native-base";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from "@react-navigation/native-stack";
+import ScanQRCode from "./pages/ScanQRCode";
+import QRCode from "./pages/QRCode";
+
+const Drawer = createDrawerNavigator();
+const HomeStack = createNativeStackNavigator();
+
+const drawerHeaderSetting = (props: {
+  route: RouteProp<ParamListBase, string>;
+  navigation: any;
+}) => ({
+  headerStyle: {
+    backgroundColor: colors.green["500"],
+  },
+  headerTintColor: colors.white,
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+  headerLeft: () => (
+    <Button
+      style={{ backgroundColor: "transparent" }}
+      onPress={() => {
+        props.navigation.openDrawer();
+      }}
+    >
+      <HamburgerIcon style={{ color: "white", marginLeft: 10, height: 22 }} />
+    </Button>
+  ),
+});
+
+const DrawScreens = () => (
+  <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Screen
+      name="Home"
+      component={Home}
+      options={(props) =>
+        ({
+          title: "Home",
+          ...drawerHeaderSetting(props),
+        } as DrawerNavigationOptions)
+      }
+    />
+    <Drawer.Screen
+      name="Notification"
+      component={Notification}
+      options={(props) =>
+        ({
+          title: "Notification",
+          ...drawerHeaderSetting(props),
+        } as DrawerNavigationOptions)
+      }
+    />
+    <Drawer.Screen
+      name="QRCode"
+      component={QRCode}
+      options={(props) =>
+        ({
+          title: "QRCode",
+          ...drawerHeaderSetting(props),
+        } as DrawerNavigationOptions)
+      }
+    />
+  </Drawer.Navigator>
+);
 
 const Navigation = () => {
-  const Drawer = createDrawerNavigator();
-
-  const headerSetting = (props: any) => ({
-    headerStyle: {
-      backgroundColor: colors.green["500"],
-    },
-    headerTintColor: colors.white,
-    headerTitleStyle: {
-      fontWeight: "bold",
-    },
-    headerLeft: () => (
-      <Button
-        style={{ backgroundColor: "transparent" }}
-        onPress={() => {
-          props.navigation.openDrawer();
-        }}
-      >
-        <HamburgerIcon style={{ color: "white", marginLeft: 10, height: 22 }} />
-      </Button>
-    ),
+  const [fontsLoaded] = useFonts({
+    "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
   });
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen
-          name="Home"
-          component={Home}
-          options={(props) => ({
-            title: "Home",
-            ...headerSetting(props),
-          })}
+      <HomeStack.Navigator initialRouteName="Drawer">
+        <HomeStack.Screen
+          name="Drawer"
+          component={DrawScreens}
+          options={() =>
+            ({
+              headerShown: false,
+            } as NativeStackNavigationOptions)
+          }
         />
-        <Drawer.Screen
-          name="Notification"
-          component={Notification}
-          options={(props) => ({
-            title: "Notification",
-            ...headerSetting(props),
-          })}
+        <HomeStack.Screen
+          name="ScanQrCode"
+          component={ScanQRCode}
+          options={(props) =>
+            ({
+              title: "掃描二維碼",
+              headerStyle: {
+                backgroundColor: "#13b188",
+              },
+              headerTintColor: colors.white,
+              headerTitleStyle: {
+                fontWeight: "bold",
+                fontFamily: fontsLoaded ? "Rubik-Regular" : "",
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 4.84,
+                elevation: 16,
+                fontSize: 18,
+              },
+              headerLeft: () => (
+                <Button
+                  style={{
+                    backgroundColor: "transparent",
+                  }}
+                  onPress={() => {
+                    props.navigation.goBack();
+                  }}
+                >
+                  <Image
+                    style={{
+                      tintColor: "white",
+                      height: 25,
+                      width: 25,
+                      bottom: 5,
+                      right: 20,
+                    }}
+                    source={require("./assets/images/qrcode/headerBack.png")}
+                  />
+                </Button>
+              ),
+            } as NativeStackNavigationOptions)
+          }
         />
-      </Drawer.Navigator>
+      </HomeStack.Navigator>
     </NavigationContainer>
   );
 };
---
+++ pages/ScanQRCode.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import BarcodeMask from "react-native-barcode-mask";
+
+export default function ScanQRCode() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
+    console.log("scanned", data, type);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={[StyleSheet.absoluteFillObject, styles.camera]}
+      >
+        <Image
+          source={require("../assets/images/qrcode/qrCodeBorder.png")}
+          style={styles.border}
+        />
+        <BarcodeMask
+          showAnimatedLine={false}
+          width={290}
+          height={290}
+          edgeColor="transparent"
+        />
+        <Text
+          style={{
+            position: "relative",
+            top: 50,
+            color: "white",
+            fontSize: 18,
+          }}
+        >
+          掃描二維碼
+        </Text>
+      </BarCodeScanner>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    position: "relative",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  overlay: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(1,1,1,0.6)",
+  },
+  border: {
+    marginTop: 20,
+    height: 300,
+    width: 300,
+    backgroundColor: "transparent",
+    zIndex: 1,
+  },
+});
